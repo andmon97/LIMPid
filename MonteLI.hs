2ce0c994c3f82192aaf3819410ee57dc2dd20533
Andrea Montemurro <andrea.montemurro97@gmail.com>
2021-02-19 17:55:37 +0100

    parser functions completed

--- MonteLI.hs
+++ MonteLI.hs
@@ -9,8 +9,8 @@ getCh           = do hSetEcho stdin False
                      hSetEcho stdin True
                      return c
 
-                     
--- Definition of the environment: couple with name and value of the variable
+
+-- Definition of the environment: couple with name and value of the variables
 type Env    =   [(String, String)]
 
 -- Definition of the Parser: in input there are the Environment and the input string, and in output a triple
@@ -35,3 +35,33 @@ parseReturn newenv v = \env inp -> [(newenv, v, inp)]
 -- "failure" stops the parsing
 failure         :: Parser a
 failure         = \env inp -> []
+
+-- "parse" executes the parsing
+parse           :: Parser a -> Env -> String ->[(Env, a, String)]
+parse p env inp = p env inp
+
+-- SEQUENCING operator (>>>=). parse p >>>= f fails if the application of the parser p to the input string fails, and otherwise applies the function f to the result value to give a
+-- second parser, which is then applied to the output string to give the final result
+(>>>=)          :: Parser a -> (Env -> a -> Parser b) -> Parser b
+p >>>= f        = \env inp -> case parse p env inp of 
+                                [] -> []
+                                [(env, v, out)] -> parse (f env v) env out
+
+-- CHOICE operator (+++). p +++ q execute p, otherwise if p fails executes q. Input and env are transferred from a parser to the other one
+(+++)           :: Parser a -> Parser a -> Parser a
+p +++ q         = \env inp -> case p env inp of
+                                [] -> parse q env inp
+                                [(env, v, out)] -> [(env, v, out)]
+
+-- Parse a character if the predicate p is satisfied
+sat             :: (Char -> Bool) -> Parser Char
+sat p           = item >>>= \env x ->
+                    if p x then
+                        parseReturn env x
+                    else 
+                        failure
+
+
+-- Parse ua specific character 
+char            :: Char -> Parser Char
+char x          = sat (x ==) 
