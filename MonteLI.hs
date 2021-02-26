171efc11929c9325a215dbc75d353b3c68833d30
Andrea Montemurro <andrea.montemurro97@gmail.com>
2021-02-26 12:35:28 +0100

    fix in keyword parsers and utility functions

--- MonteLI.hs
+++ MonteLI.hs
@@ -23,48 +23,49 @@ type Parser a = Env -> String -> [(Env, a, String)]
 --                  PARSER FUNCTIONS                --
 -- _____________________________________________________________________________________________________________________________________--
 -- "item" parses the first element of a list and return the rest of the list
-item            :: Parser Char
-item            = \env inp -> case inp of
-                            [] -> []
-                            (x:xs) -> [(env, x, xs)]
-
+item :: Parser Char
+item = \env inp -> case inp of
+                   []     -> []
+                   (x:xs) -> [(env,x,xs)] 
+                   
 -- "parserReturn" modify the environment after an assignment
 -- return a Parser after sostituting the elements of the expression a and Env of the triple with the input values
-parserReturn          :: Env -> a -> Parser a
-parserReturn newenv v = \env inp -> [(newenv, v, inp)]
+parserReturn  :: Env -> a -> Parser a
+parserReturn newenv v = \env inp -> [(newenv, v,inp)]
 
 -- "failure" stops the parsing
-failure         :: Parser a
-failure         = \env inp -> []
-
--- "parse" executes the parsing
-parse           :: Parser a -> Env -> String ->[(Env, a, String)]
-parse p env inp = p env inp
-
--- SEQUENCING operator (>>>=). parse p >>>= f fails if the application of the parser p to the input string fails, and otherwise applies the function f to the result value to give a
--- second parser, which is then applied to the output string to give the final result
-(>>>=)          :: Parser a -> (Env -> a -> Parser b) -> Parser b
-p >>>= f        = \env inp -> case parse p env inp of 
-                                [] -> []
-                                [(env, v, out)] -> parse (f env v) env out
+failure :: Parser a
+failure  = \env inp -> []
 
 -- CHOICE operator (+++). p +++ q execute p, otherwise if p fails executes q. Input and env are transferred from a parser to the other one
-(+++)           :: Parser a -> Parser a -> Parser a
-p +++ q         = \env inp -> case p env inp of
-                                [] -> parse q env inp
-                                [(env, v, out)] -> [(env, v, out)]
+(+++)  :: Parser a -> Parser a -> Parser a
+p +++ q = \env inp -> case p env inp of
+                   []        -> parse q env inp
+                   [(env, v,out)] -> [(env, v,out)]
+
+-- Executes the parsing          
+parse :: Parser a -> Env -> String -> [(Env, a,String)]
+parse p env inp = p env inp
+             
+-- SEQUENCING operator (>>>=). parse p >>>= f fails if the application of the parser p to the input string fails, and otherwise applies the function f to the result value to give a
+-- second parser, which is then applied to the output string to give the final result
+(>>>=) :: Parser a -> (Env -> a -> Parser b ) -> Parser b
+p >>>= f = \env inp -> case parse p env inp of
+                       [] -> []
+                       [(env, v, out)] -> parse (f env v) env out
 
 -- Parse a character if the predicate p is satisfied
-sat             :: (Char -> Bool) -> Parser Char
-sat p           = item >>>= \env x ->
-                    if p x then
-                        parserReturn env x
-                    else 
-                        failure
+sat  :: (Char -> Bool) -> Parser Char
+sat p = item >>>= \env x ->
+           if p x then
+             parserReturn env x
+           else
+             failure
 
--- Parse ua specific character 
-char            :: Char -> Parser Char
-char x          = sat (x ==) 
+-- Parse a specific character
+char  :: Char -> Parser Char
+char x = sat (x ==) 
+
 
 
 -- _____________________________________________________________________________________________________________________________________--
@@ -74,43 +75,45 @@ char x          = sat (x ==)
 - v is the NAME of the variable
 - a is the VALUE of the var (String type because it will be substituted in the string code)
 - es is the environment -}
-setEnv         :: String -> String -> Env -> Env 
-setEnv v a []  = [(v, a)]
-setEnv v a (e:es)
-                | (fst e) == v  = [(v, a)] ++ es 
-                | otherwise     = e: (setEnv v a es)
-
-{- "replace" repl. a variable in the environment
-- v is the couple name,value of the variable
-- xs is the expression to evaluate -}
-replace        :: (String, String) -> String -> String
-replace v []   = []
-replace v xs 
-    | (fst v) `isPrefixOf` xs = (snd v) ++ replace v (drop (length (fst v)) xs)
-    | otherwise = (xs !! 0) : replace v (drop 1 xs)
+setEnv :: String -> String -> Env -> Env
+setEnv v a []  = [(v,a)]
+setEnv v a (e:es)    
+              | (fst e)==v      = [(v,a)] ++ es
+              | otherwise       = e:(setEnv v a es)
 
 {- "bind" is a function that interpeters all the variable in the env
 -- es enviroment of variables
 -- xs expression to evaluate -}
-bind           :: Env -> String -> String 
-bind [] xs     = xs
-bind es []     = []
+bind ::  Env -> String -> String
+bind [] xs = xs
+bind es [] = []
 bind (e:es) xs = bind es (replace e xs) 
 
--- "getCode" extract the code (type of a) from the tuple --
-getCode             :: [(Env, a, String)] -> a 
-getCode [(_,x,_)]   = x 
+{- "replace" replaces a variable in the environment
+- v is the couple name,value of the variable
+- xs is the expression to evaluate -}
+replace :: (String,String) -> String -> String
+replace v [] = []
+replace v xs
+   | (fst v) `isPrefixOf` xs = (snd v) ++ replace v (drop (length (fst v)) xs)
+   | otherwise = (xs!!0) : replace v (drop 1 xs)
 
--- extract the env state (is like a toString method) -- 
-getMemory               :: [(Env, a, String)] -> String 
-getMemory []            = []
-getMemory [([],_,_)]    = []
+-- "getCode" extract the code (type of a) from the tuple --
+getCode :: [(Env, a, String)] -> a
+getCode [(_, x, _)]  =  x
+
+-- extract the env state -- 
+getMemory :: [(Env, a, String)] -> String
+getMemory []             = []
+getMemory [([], _, _)]   = [] 
+getMemory [(x:xs, c, s)] = fst x ++ "=>" ++ snd x ++ " " ++ (getMemory [(xs,c,s)])
 
 -- extract the environment from the tuple --
-getEnv                  :: [(Env, a, String)] -> Env 
-getEnv []               = []
-getEnv [([],_,_)]       = []
-getEnv [(x,_,_)]        = x 
+getEnv :: [(Env, a, String)] -> Env
+getEnv []            = []
+getEnv [([], _, _)]  = [] 
+getEnv [(x, _, _)]   = x
+
 
 
 -- _____________________________________________________________________________________________________________________________________--
@@ -118,77 +121,76 @@ getEnv [(x,_,_)]        = x
 -- _____________________________________________________________________________________________________________________________________--
 
 -- Parse the "True" keyword
-trueKeyword     :: Parser String
-trueKeyword     = char 'T' >>>= \env _ -> char 'r' >>>= \_ _ -> char 'u' >>>= \_ _ -> char 'e' >>>= \_ _ -> parserReturn env ("True")
+trueKeyword :: Parser String
+trueKeyword  = char 'T' >>>= \env _ -> char 'r' >>>= \_ _ ->  char 'u' >>>= \_ _ -> char 'e' >>>= \_ _ -> parserReturn env ("True")
 
 -- Parse the "False" keyword
-falseKeyword    :: Parser String
-falseKeyword    = char 'F' >>>= \env _ -> char 'a' >>>= \_ _ -> char 'l' >>>= \_ _ -> char 's' >>>= \_ _ -> char 'e' >>>= \_ _ -> parserReturn env ("False")
+falseKeyword :: Parser String
+falseKeyword  = char 'F' >>>= \env _ -> char 'a' >>>= \_ _ -> char 'l' >>>= \_ _ -> char 's' >>>= \_ _ -> char 'e' >>>= \_ _ ->  parserReturn env ("False")
 
--- Parse the "skip" keyword
-skipKeyword     :: Parser String
-skipKeyword     = char 's' >>>= \env _ -> char 'k' >>>= \_ _ -> char 'i' >>>= \_ _ -> char 'p' >>>= \_ _ -> parserReturn env ("skip")
+-- Parse the ""skip"" keyword
+skipKeyword :: Parser String
+skipKeyword  = char 's' >>>= \env _ -> char 'k' >>>= \_ _ -> char 'i' >>>= \_ _ -> char 'p' >>>= \_ _ -> parserReturn env ("skip")
 
 -- Parse the "if" keyword
-ifKeyword       :: Parser String
-ifKeyword       = char 'i' >>>= \env _ -> char 'f' >>>= \_ _ -> space >>>= \_ _ -> parserReturn env ("if ")
-
+ifKeyword :: Parser String
+ifKeyword  = char 'i' >>>= \env _ ->  char 'f' >>>= \_ _ -> space >>>= \_ _ -> parserReturn env "if "
+              
 -- Parse the "else" keyword
-elseKeyword     :: Parser String
-elseKeyword     = char 'e' >>>= \env _ -> char 'l' >>>= \_ _ -> char 's' >>>= \_ _ -> char 'e' >>>= \_ _ -> space >>>= \_ _ -> parserReturn env ("else ")
+elseKeyword :: Parser String
+elseKeyword  =  char 'e' >>>= \env _ -> char 'l' >>>= \_ _ -> char 's' >>>= \_ _ -> char 'e' >>>= \_ _ -> space >>>= \_ _ -> parserReturn env "else "
 
--- Parse the "do" keyword
-doKeyword       :: Parser String
-doKeyword       = char 'd' >>>= \env _ -> char 'o' >>>= \_ _ -> space >>>= \_ _ -> parserReturn env ("do ")
+-- Parse the "while" keyword                        
+whileKeyword :: Parser String
+whileKeyword  = char 'w' >>>= \env _ -> char 'h' >>>= \_ _ -> char 'i' >>>= \_ _ -> char 'l' >>>= \_ _ -> char 'e' >>>= \_ _ -> space >>>= \_ s -> parserReturn env "while "
 
--- Parse the "while" keyword
-whileKeyword    :: Parser String
-whileKeyword    = char 'w' >>>= \env _ -> char 'h' >>>= \_ _ -> char 'i' >>>= \_ _ -> char 'l'  >>>= \_ _ -> char 'e' >>>= \_ _ -> space >>>= \_ _ -> parserReturn env ("while ")
-
+-- Parse the "do" keyword                    
+doKeyword :: Parser String
+doKeyword  = char 'd' >>>= \env _ -> char 'o' >>>= \_ _ -> space >>>= \_ _ -> parserReturn env "do "
+                        
 --      chars parsers
--- Parse the opened pargraf
-openPargraf     :: Parser String 
-openPargraf     = char '{' >>>= \env _ -> space >>>= \_ _ -> parserReturn env ("{ ")
+-- Parse the opened graf parentheses
+openPargraf :: Parser String
+openPargraf   = char '{' >>>= \env _ -> space >>>= \_ _ -> parserReturn env ("{ ")--
 
--- Parse the closed pargraf
-closePargraf   :: Parser String
-closePargraf   = char '}' >>>= \env _ -> parserReturn env ("}")
+-- Parse the closed graf parentheses
+closePargraf :: Parser String
+closePargraf   = char '}' >>>= \env _ ->  parserReturn env ("}")  --                                         
 
--- Parse the opened square parenth.
-openPar        :: Parser String 
-openPar        = char '(' >>>= \env _ -> parserReturn env (")")
+-- Parse open square parenthesis (with a possible space prefix)
+openPar :: Parser String
+openPar = char '(' >>>= \env _ -> parserReturn env "("
 
--- Parse the closed square parenth.
-closePar        :: Parser String 
-closePar        = char ')' >>>= \env _ -> space >>>= \_ _ -> parserReturn env (")")
+-- Parse closed square parenthesis (with a possible space prefix)
+closePar :: Parser String
+closePar = char ')' >>>= \env _ -> space >>>= \_ _ -> parserReturn env ") "
 
--- Parse the colon
-colon           :: Parser String
-colon           = char ',' >>>= \env _ ->
-                                        (
-                                            space >>>= \_ _ ->
-                                            parserReturn env ", "
-                                        )
-                                        +++
-                                        parserReturn env ","
+-- Parse colon
+colon :: Parser String
+colon = char ',' >>>= \env _ -> 
+             (
+              space >>>= \_ _ -> 
+                parserReturn env ", "
+              ) +++
+              parserReturn env ","
 
--- Parse the semicolon
-semicolon       :: Parser String
-semicolon       = char ',' >>>= \env _ ->
-                                        (
-                                            space >>>= \_ _ ->
-                                            parserReturn env "; "
-                                        )
-                                        +++
-                                        parserReturn env ";"
+-- Parse semicolon
+semicolon :: Parser String
+semicolon = char ';' >>>= \env _ -> 
+              (
+              space >>>= \_ _ -> 
+              parserReturn env "; "--
+              ) +++
+              parserReturn env ";"
 
--- Parse the space character
-space           :: Parser Char
-space           = char ' '
+-- Parse a space character
+space :: Parser Char
+space = char ' '
 
--- Parse a string of spaces
-spaces          :: Parser String 
-spaces          = char ' ' >>>= \env _ -> parserReturn env " "
+-- Parse a string of more spaces 
+spaces :: Parser String
+spaces = char ' ' >>>= \env _ -> 
+           parserReturn env " "
 
 
 
